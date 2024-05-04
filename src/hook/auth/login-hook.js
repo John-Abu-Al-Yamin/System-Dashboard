dd37733caace6c8c95637729218a21672d79c59b
John-Abu-Al-Yamin <je0409389@gmaill.com>
2024-05-04 12:33:41 +0200

    improve-1

--- src/hook/auth/login-hook.js
+++ src/hook/auth/login-hook.js
@@ -1,65 +1,76 @@
-import React, { useState, useEffect } from 'react'
- import { useDispatch, useSelector } from 'react-redux';
-import { adminLogin } from '../../store/auth/authSlice';
- 
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { adminLogin } from "../../store/auth/authSlice";
+import { toast } from "react-toastify";
+
 const LoginHook = () => {
-    const dispatch = useDispatch();
-  
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [loading, setLoading] = useState(true)
-    const [isPress, setIsPress] = useState(false)
-    const onChangeEmail = (e) => {
-        setEmail(e.target.value)
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [isPress, setIsPress] = useState(false);
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      toast.error("Please complete your data");
+      console.log("Please complete your data");
     }
 
-    const onChangePassword = (e) => {
-        setPassword(e.target.value)
-    }
+    setIsPress(true);
+    setLoading(true);
+    await dispatch(
+      adminLogin({
+        email,
+        password,
+      })
+    );
 
-    const onSubmit = async (e) => {
-      e.preventDefault();
-      
-        setIsPress(true)
-        setLoading(true)
-        await dispatch(adminLogin({
-            email,
-            password
-        }))
-
-        setLoading(false)
-        setIsPress(false)
-    }
-    const res = useSelector(state => state.auth.loginUser)
-    useEffect(() => {
-        if (loading === false) {
-            if (res) {
-                 if (res.data.token) {
-                  console.log(res.data.token)
-                  console.log(res.data)
-
-                    localStorage.setItem("token", res.data.token)
-                    localStorage.setItem("user", JSON.stringify(res.data.data))
-                     setTimeout(() => {
-                        window.location.href = "/"
-                    }, 1500);
-                } else {
-                    localStorage.removeItem("token")
-                    localStorage.removeItem("user")
-                }
-
-                if (res.data.message === "Incorrect email or password") {
-                    localStorage.removeItem("token")
-                    localStorage.removeItem("user")
-                    console.log("كلمة السر او الايميل خطا")
-
-                 }
-                setLoading(true)
-            }
+    setLoading(false);
+    setIsPress(false);
+  };
+  const res = useSelector((state) => state.auth.loginUser);
+  useEffect(() => {
+    if (loading === false) {
+      if (res) {
+        if (res.data.token) {
+          localStorage.setItem("token", res.data.token);
+          localStorage.setItem("user", JSON.stringify(res.data.data));
+          setTimeout(() => {
+            window.location.href = "/";
+          }, 1500);
+        } else {
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
         }
-    }, [loading])
 
-    return [email, password, loading, onChangeEmail, onChangePassword, onSubmit, isPress]
-}
+        if (res?.data.message === "Incorrect email or password") {
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
+          toast.error("Incorrect email or password");
+        }
+        setLoading(true);
+      }
+    }
+  }, [loading]);
 
-export default LoginHook
+  return [
+    email,
+    password,
+    loading,
+    onChangeEmail,
+    onChangePassword,
+    onSubmit,
+    isPress,
+  ];
+};
+
+export default LoginHook;
