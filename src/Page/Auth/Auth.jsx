fd814870ed6950e46427af6d9766111e555e27de
John-Abu-Al-Yamin <149198898+John-Abu-Al-Yamin@users.noreply.github.com>
2024-06-25 21:17:46 +0300

    Update Auth lodding

--- src/Page/Auth/Auth.jsx
+++ src/Page/Auth/Auth.jsx
@@ -77,7 +77,18 @@ const Auth = () => {
                     }`}
                     disabled={!loading}
                   >
-                    {loading && isPress ? "Loading..." : "Sign in"}
+                    {loading && isPress ? <>
+                      <div class="text-center">
+  <div
+    class="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-500 mx-auto"
+  ></div>
+  <h2 class="text-zinc-900 dark:text-white mt-4">Loading...</h2>
+  <p class="text-zinc-600 dark:text-zinc-400">
+    Your adventure is about to begin
+  </p>
+</div>
+
+                    </> : "Sign in"}
                   </button>
                 </form>
                 <p className="text-white mt-10 text-xs text-center">
